bfea87b4dc3e1e9f866b77972a68685cfedf3005
huryaq <hurryapurrya@gmail.com>
2025-08-20 12:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Mitsuri_Pro_File_Bot")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/y-w.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "https://envs.sh/ufx.jpg").split() #Required
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
